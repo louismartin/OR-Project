74cf1bfb5c8c7cdf1ce65589282ce1b78f9871d9
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-29 12:08:29 +0100

    put single image process in function

--- image_processing.py
+++ image_processing.py
@@ -12,9 +12,42 @@ ann_file = '{0}/annotations/instances_{1}.json'.format(data_dir, data_type)
 ann_file = op.join(
     data_dir, "annotations", "instances_{0}.json".format(data_type))
 output_shape = (244, 244, 3)
+categories = ["tennis", "boat", "giraffe"]
 
 
-def load_images():
+def process_image(img):
+    '''From an image given by the MS Coco API, returns the ndarray standardized
+    if the image is tractable, False o.w.
+        Args:
+            - img (dict): the image dictionary returned by the MS Coco API
+        Output:
+            - ndarray: the matrix associated with the cropped and resized image
+    '''
+    img_mat = misc.imread(
+        op.join(data_dir, data_type, img["file_name"]))
+    # We need to crop image on both sides in order to have
+    # standardized images in terms of shape
+    dimensions = img_mat.shape
+    if len(dimensions) == 3:
+        bigger_dim = np.argmax(dimensions[:2])
+        smaller_dim = 1 - bigger_dim
+        difference = dimensions[bigger_dim] -\
+            dimensions[smaller_dim]
+        if difference > 0:
+            left_crop = math.floor(difference / 2)
+            right_crop = math.ceil(difference / 2)
+            if bigger_dim == 0:
+                X = img_mat[left_crop:-right_crop, :, :]
+            else:
+                X = img_mat[:, left_crop:-right_crop, :]
+        else:
+            X = img_mat
+        return misc.imresize(X, output_shape)
+    else:
+        return False
+
+
+def load_images(categories=None):
     '''Preprocessing of the MS Coco dataset
         Output:
             - ndarray (n, 244, 244, 3): the input data (x_train), images
@@ -24,43 +57,27 @@ def load_images():
     Y_train = list()
     # initialize COCO api for instance annotations
     coco = COCO(ann_file)
-    # load all categories
-    cats = coco.loadCats(coco.getCatIds())
-    names = [cat["name"] for cat in cats]
+    if categories:
+        # load all categories
+        cats = coco.loadCats(coco.getCatIds())
+        names = [cat["name"] for cat in cats]
+    else:
+        names = categories
     cat_ids = coco.getCatIds(catNms=names)
     registered_img_ids = set()
     for cat_id in cat_ids:
         # Get all the image ids related to that category
         img_ids = set(coco.catToImgs[cat_id])
         for img_id in img_ids:
-
             if not(img_id in registered_img_ids):
                 # for each image we haven't processed yet, we process it
                 img = coco.loadImgs(img_id)[0]
-                img_mat = misc.imread(
-                    op.join(data_dir, data_type, img["file_name"]))
-                # We need to crop image on both sides in order to have
-                # standardized images in terms of shape
-                dimensions = img_mat.shape
-                if len(dimensions) == 3:
-                    bigger_dim = np.argmax(dimensions[:2])
-                    smaller_dim = 1 - bigger_dim
-                    difference = dimensions[bigger_dim] -\
-                        dimensions[smaller_dim]
-                    if difference > 0:
-                        left_crop = math.floor(difference / 2)
-                        right_crop = math.ceil(difference / 2)
-                        if bigger_dim == 0:
-                            X = img_mat[left_crop:-right_crop, :, :]
-                        else:
-                            X = img_mat[:, left_crop:-right_crop, :]
-                    else:
-                        X = img_mat
-                    X = misc.imresize(X, output_shape)
+                X = process_image(img)
+                if X:
                     X_train = X_train, X
                     Y_train = Y_train, coco.loadCats(cat_id)[0]["name"]
         registered_img_ids = img_ids.union(img_ids)
     X_train = np.array(X_train)
     return X_train, Y_train
 
-load_images()
+load_images(categories)
