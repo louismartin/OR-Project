a90e2569d09fe41a775c58f8f46ebbc2bd4ef94c
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-29 17:08:08 +0100

    build pipeline for vgg features

--- vgg.py
+++ vgg.py
@@ -1,4 +1,18 @@
-import numpy as np
 from keras.applications import vgg19
+from keras import backend as K
+from keras.optimizers import SGD
+import numpy as np
+
 
 net = vgg19.VGG19()
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
+
+def compile_net():
+    net.compile(optimizer=sgd, loss='categorical_crossentropy')
+
+
+def compute_vgg_features(X, layer=2):
+    f = K.function([net.layers[0].input, K.learning_phase()],
+                   [net.layers[-layer].output])
+    return f([im, 0])[0]
