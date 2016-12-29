589de71e467170124df4b3c5acc82b7042b924ca
zaccharieramzi <zaccharie.ramzi@gmail.com>
2016-12-29 11:52:56 +0100

    refactored code

--- image_processing.py
+++ image_processing.py
@@ -1,13 +1,16 @@
 import math
+import os.path as op
 import time
 
 import numpy as np
 from pycocotools.coco import COCO
 from scipy import misc
 
-data_dir = 'dataset'
-data_type = 'train2014'
+data_dir = "dataset"
+data_type = "train2014"
 ann_file = '{0}/annotations/instances_{1}.json'.format(data_dir, data_type)
+ann_file = op.join(
+    data_dir, "annotations", "instances_{0}.json".format(data_type))
 output_shape = (244, 244, 3)
 
 
@@ -17,14 +20,14 @@ def load_images():
             - ndarray (n, 244, 244, 3): the input data (x_train), images
             - list (n): the labels of the images (y_train)
     '''
-    x_train = list()
-    y_train = list()
+    X_train = list()
+    Y_train = list()
     # initialize COCO api for instance annotations
     coco = COCO(ann_file)
     # load all categories
     cats = coco.loadCats(coco.getCatIds())
-    nms = [cat['name'] for cat in cats]
-    cat_ids = coco.getCatIds(catNms=nms)
+    names = [cat["name"] for cat in cats]
+    cat_ids = coco.getCatIds(catNms=names)
     registered_img_ids = set()
     for cat_id in cat_ids:
         # Get all the image ids related to that category
@@ -34,13 +37,12 @@ def load_images():
             if not(img_id in registered_img_ids):
                 # for each image we haven't processed yet, we process it
                 img = coco.loadImgs(img_id)[0]
-                img_mat = misc.imread('/'.join(
-                    [data_dir, data_type, img['file_name']]))
+                img_mat = misc.imread(
+                    op.join(data_dir, data_type, img["file_name"]))
                 # We need to crop image on both sides in order to have
                 # standardized images in terms of shape
                 dimensions = img_mat.shape
                 if len(dimensions) == 3:
-                    tic = time.time()
                     bigger_dim = np.argmax(dimensions[:2])
                     smaller_dim = 1 - bigger_dim
                     difference = dimensions[bigger_dim] -\
@@ -49,18 +51,16 @@ def load_images():
                         left_crop = math.floor(difference / 2)
                         right_crop = math.ceil(difference / 2)
                         if bigger_dim == 0:
-                            x = img_mat[left_crop:-right_crop, :, :]
+                            X = img_mat[left_crop:-right_crop, :, :]
                         else:
-                            x = img_mat[:, left_crop:-right_crop, :]
+                            X = img_mat[:, left_crop:-right_crop, :]
                     else:
-                        x = img_mat
-                    x = misc.imresize(x, output_shape)
-                    x_train = x_train, x
-                    y_train = y_train, coco.loadCats(cat_id)[0]['name']
-                    toc = time.time()
-                    print(toc - tic)
+                        X = img_mat
+                    X = misc.imresize(X, output_shape)
+                    X_train = X_train, X
+                    Y_train = Y_train, coco.loadCats(cat_id)[0]["name"]
         registered_img_ids = img_ids.union(img_ids)
-    x_train = np.array(x_train)
-    return x_train, y_train
+    X_train = np.array(X_train)
+    return X_train, Y_train
 
 load_images()
