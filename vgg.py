4536eeb841033d20c3075efbd905d06abd622a2b
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-01-07 10:47:38 +0100

    review vgg documentation

--- vgg.py
+++ vgg.py
@@ -3,16 +3,16 @@ import numpy as np
 
 
 def compute_nn_features(X, net, layer=2):
-    """Computes features of a batch of image matrices from a given layer of a
+    '''Computes features of a batch of image matrices from a given layer of a
     given neural network
         Args:
             - X (ndarray: (None, 224, 224, 3)): the batch of matrices
-            - net (keras.Model): the neural network
+            - net (keras model): the neural network
             - layer (int): the index of the layer (starting from the output)
             you want to consider
         Output:
             - ndarray: (None, 4096): the vgg features for the batch of matrices
-    """
+    '''
     f = K.function([net.layers[0].input, K.learning_phase()],
                    [net.layers[-layer].output])
     return f([X, 0])[0]
