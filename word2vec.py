e3aaf5d3379978444d3768811a270a31f99db455
Louis MARTIN <louisrtm@gmail.com>
2017-01-07 10:40:35 +0100

    Create compute_textual_feature function

--- word2vec.py
+++ word2vec.py
@@ -1,7 +1,4 @@
-
 # coding: utf-8
-
-# Download Google's pretrained model $\href{https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing}{here}$
 
 import re
 import string
@@ -15,23 +12,25 @@ from tqdm import tqdm
 from pycocotools.coco import COCO
 from text_processing import create_caption_dataframe, sentence2vec
 
+def compute_textual_features(df_caption, overwrite=False):
+    textual_embeddings_path = op.join('dataset', 'annotations', 'textual_embeddings.npy')
+    if op.exists(textual_embeddings_path) and not overwrite:
+        X = np.load(textual_embeddings_path)
+    else:
+        ### Load Google's pre-trained Word2Vec model.
+        # https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
+        print('\nLoading word2vec model ...')
+        path = op.join('models', 'GoogleNews-vectors-negative300.bin')
+        model = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
 
-df_caption = create_caption_dataframe()
 
-
-### Load Google's pre-trained Word2Vec model.
-# https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
-print('\nLoading word2vec model ...')
-path = op.join('models', 'GoogleNews-vectors-negative300.bin')
-model = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
-
-
-# Compute every caption vector representation
-i = 0
-X = np.zeros((df_caption.shape[0], model.vector_size))
-for index, row in tqdm(df_caption.iterrows(), total=df_caption.shape[0],
-                        desc='Computing textual embeddings'):
-    X[i,:] = sentence2vec(row['caption'], model)
-    i += 1
-np.save('dataset/annotations/textual_embeddings.npy', X)
+        # Compute every caption vector representation
+        i = 0
+        X = np.zeros((df_caption.shape[0], model.vector_size))
+        for index, row in tqdm(df_caption.iterrows(), total=df_caption.shape[0],
+                                desc='Computing textual embeddings'):
+            X[i,:] = sentence2vec(row['caption'], model)
+            i += 1
+        np.save(textual_embeddings_path, X)
+    return X
 
