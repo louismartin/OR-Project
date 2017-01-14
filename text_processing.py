e81d4ca99980956c14db31d0e5c09636247bee82
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-01-14 16:16:17 +0100

    remove stop words from textual features computation

--- text_processing.py
+++ text_processing.py
@@ -3,6 +3,7 @@ import string
 import os.path as op
 
 import gensim
+from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -11,9 +12,8 @@ from pycocotools.coco import COCO
 
 # Regex to remove punctuation
 regex = re.compile('[%s]' % re.escape(string.punctuation))
-stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 # Remove punctuation and convert to set
-stopwords = set([regex.sub('', word) for word in stopwords])
+stops = set(stopwords.words('english'))
 
 
 def sentence2vec(sentence, model):
@@ -30,7 +30,7 @@ def sentence2vec(sentence, model):
     sentence = sentence.lower()
     sentence = regex.sub('', sentence)
     words = sentence.split(' ')
-    words = [word for word in words if (word not in stopwords)]
+    words = [word for word in words if (word not in stops)]
 
     # Average vector representations of the words in the sentence
     vec = np.zeros(model.vector_size)
@@ -45,7 +45,7 @@ def sentence2vec(sentence, model):
 
 def create_caption_dataframe(overwrite=False):
     '''
-    Create a dataframe containing all captions for each images 
+    Create a dataframe containing all captions for each images
     '''
     # initialize COCO api for caption annotations
     data_dir='dataset'
